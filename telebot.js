b89d3e5d4224064278e6e79a6f8f9518029b748a
Kyujin Cho <thy2134@gmail.com>
2016-11-22 11:22:47 +0900

    Add search feature

--- telebot.js
+++ telebot.js
@@ -1,4 +1,6 @@
 var TelegramBot = require('node-telegram-bot-api')
+var files = require('../DB/File.js')
+
 var confs = require('./confs.js')
 
 var link = confs.link
@@ -10,16 +12,45 @@ bot.onText(/\/upload/, function(msg, match) {
 })
 
 bot.onText(/\/files/, function(msg, match) {
-	bot.sendMessage(msg.chat.id, 'http://' + link + '/files')
+	var str = ''
+	files.find({}, function(err, file) {
+		file.forEach(f => function() {
+			f = f.toObject();
+			str += f.name + ' : http://' + link + '/files/' + f.name + '\n';
+			str += 'Uploader : ' + f.uploader + '\n\n';
+		}).then(function(doc) {
+			if(str == '') {
+				bot.sendMessage(msg.chat.id, '파일이 없습니다. ');
+			} else {
+				bot.sendMessage(msg.chat.id, '파일 목록: \n' + str);
+			}
+		})
+	})
 })
 
-
+bot.onText(/\/search (.+)/. function(msg, match) {
+	var kwd = match[1];
+	var str = ''
+	files.find({name : kwd}, function(err, file) {
+		file.forEach(f => function() {
+			f = f.toObject();
+			str += f.name + ' : http://' + link + '/files/' + f.name + '\n';
+			str += 'Uploader : ' + f.uploader + '\n\n';
+		}).then(function(doc) {
+			if(str == '') {
+				bot.sendMessage(msg.chat.id, '검색 결과가 없습니다. ');
+			} else {
+				bot.sendMessage(msg.chat.id, '검색 결과: \n' + str);
+			}
+		})
+	})
+})
 var sendMessage = function(uploader, link) {
 	chatIds.forEach(function(element, index, array) {
 		if(uploader == '' || uploader == null) { 
 			bot.sendMessage(element, '저장소에 파일이 업로드되었습니다.\n링크: http://' + link);
 		} else {
-			bot.sendMessage(element, '@' + uploader + ' 님이 파일을 전송하셨습니다.\n링크: ' + link);		
+			bot.sendMessage(element, '@' + uploader + ' 님이 파일을 업로드 하였습니다.\n링크: http://' + link);		
 		}
 	})
 }
