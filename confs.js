0419f4b0de848731c4d772b927e81ae001b166b2
Kyujin Cho <thy2134@gmail.com>
2016-11-22 10:27:58 +0900

    Add port # to confs

--- confs.js
+++ confs.js
@@ -3,9 +3,11 @@ var token = 'YOUR_BOT_TOKEN'
 var link = 'your.server.link'
 var chatIds = []
 var db_name ='your_db_name'
+var port_no = 'p0rtno'
 
 module.exports.folder_location = folder_location
 module.exports.token = token
 module.exports.link = link
 module.exports.chatIds = chatIds
 module.exports.db_name = db_name
+module.exports.port_no = port_no
